180c5293fe5170b1d3a51e601d0c22d35eb6e7f1
José Antônio <joseantonio1@hotmail.com.br>
2023-09-03 17:23:56 -0300

    feat: added mor tests and code to increase coverage

--- sum_test.go
+++ sum_test.go
@@ -17,3 +17,19 @@ func TestSub(t *testing.T) {
 		t.Error("Result must be -1")
 	}
 }
+
+func TestMult(t *testing.T) {
+	result := mlt(2,3)
+
+	if result != 6 {
+		t.Error("Result must be 6")
+	}
+}
+
+func TestDiv(t *testing.T) {
+	result := div(6,2)
+
+	if result != 3 {
+		t.Error("Result must be 3")
+	}
+}
